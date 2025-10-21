d435e2c0c61e32921cce943fc6e6b5f7bc57acf3
Svea Siepe <svea.siepe@gmail.com>
2025-10-21 16:48:46 +0200

    view public lists & navbar in progress

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,7 +3,7 @@ class ListsController < ApplicationController
   before_action :set_list, only: [:show]
 
   def index
-    @lists=List.all
+    @lists=List.where(is_public: true)
   end
 
   def show
