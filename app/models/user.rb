8686fea6daa639945e57d85bdd27155e851b6a54
Vanessa Deol <vanessa.deol@outlook.de>
2025-10-21 10:57:16 +0200

    associations fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :chats, :lists
+  has_many :chats
+  has_many :lists
 end
