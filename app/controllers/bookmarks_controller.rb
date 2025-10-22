564cd36fe17ce4a7cc441ba0eb2fd6a1622edaf4
Ben Lang <ben.lang@mail.ru>
2025-10-22 15:50:04 +0200

    controller debug2

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -14,8 +14,9 @@ class BookmarksController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
 
-    def destroy
+  def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
     redirect_to list_path(@bookmark.list), notice: "Movie removed from list."
