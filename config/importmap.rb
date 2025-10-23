3f326f2d3cac3571f3d29d774f09cfe230ee59c6
Ben Lang <ben.lang@mail.ru>
2025-10-23 14:37:37 +0200

    deletebuttonfixandimportmapFix

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,8 @@
 # Pin npm packages by running ./bin/importmap
-
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap" # @5.3.8
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.esm.min.js"
+pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/esm/index.js"
