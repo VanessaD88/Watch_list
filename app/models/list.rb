473028192be8a6552f601e484d1f20e3d4972b01
Alexander Haardt <haardt.alexander@gmail.com>
2025-10-23 18:46:31 +0200

    Basic List validations

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,5 @@ class List < ApplicationRecord
   belongs_to :user
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  validates :name, :description, presence: true
 end
